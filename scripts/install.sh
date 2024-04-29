d9acdc6ef98ece0fa1a093838c1d42f9e175ff51
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-04-29 07:47:58 +0000

    Add files via upload

---
+++ scripts/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Installing AUR helper..."
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin || exit
+makepkg -si --noconfirm
+cd ..
+rm -rf paru-bin
+
+echo "Installing packages"
+paru -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft noto-fonts alacritty mpv libxinerama xorg-fonts-misc picom lxappearance brightnessctl pavucontrol playerctl pamixer flameshot otf-font-awesome polkit-gnome xclip feh xtrlock gnome-themes-extra papirus-icon-theme blueman
+sudo systemctl enable bluetooth
+
+# Thunar
+./thunar.sh
+clear
+cd 
+
+# Apps
+paru -S obsidian signal-desktop syncthing keepassxc ttf-indic-otf
+paru librewolf-bin
+clear
+paru -S --noconfirm qview
+clear
+paru -S --noconfirm vscodium-bin
+clear
+paru -S --noconfirm tor-browser-bin
+clear
+
+cd
+git clone https://github.com/mashrukk/dwm
+mv dwm/ wm
+
+echo "Installing dwm, dmenu, and slstatus..."
+cd wm/dwm || exit
+sudo make clean install
+cd ../dmenu || exit
+sudo make clean install
+cd ../slstatus || exit
+sudo make clean install
+cd
+mv wm/xinitrc ~/.xinitrc
+
+echo "Setting up picom"
+cd
+cd /etc/xdg/ || exit
+sudo rm picom.conf
+cd
+sudo mv wm/picom.conf /etc/xdg/
+cd
+mv wm/alacritty ~/.config/
+cd
+mkdir Pictures
+cd Pictures
+mkdir wallpapers
+cd
+mv wm/wall.jpg Pictures/wallpapers
+mkdir Videos
+mkdir Documents
+cd Documents
+mkdir Shared
+cd
+
+echo "DONE "
---
+++ scripts/wallpaper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Directory containing wallpapers
+wallpaper_dir="$HOME/Pictures/wallpapers"
+
+# Check if feh is installed
+if ! command -v feh &> /dev/null; then
+    echo "Error: feh is not installed."
+    exit 1
+fi
+
+# Check if the wallpaper directory exists
+if [ ! -d "$wallpaper_dir" ]; then
+    echo "Error: Wallpaper directory does not exist."
+    exit 1
+fi
+
+# Function to display wallpaper options using dmenu
+show_wallpaper_options() {
+    # List all image files in the wallpaper directory
+    wallpapers=$(find "$wallpaper_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -exec basename {} \; | sort)
+
+    # Show wallpaper options using dmenu
+    chosen_wallpaper=$(echo "$wallpapers" | dmenu -l 10)
+    echo "$chosen_wallpaper"
+}
+
+# Main function
+main() {
+    # Display wallpaper options and set the chosen wallpaper
+    chosen_wallpaper=$(show_wallpaper_options)
+
+    # Set the chosen wallpaper using feh
+    if [ -n "$chosen_wallpaper" ]; then
+        feh --bg-fill "$wallpaper_dir/$chosen_wallpaper"
+    fi
+}
+
+main
