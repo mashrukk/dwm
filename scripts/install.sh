a3e8be3ed5eafc595361433ca7a4f5d90ca53425
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-05-04 11:24:04 +0000

    Added emoji font

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ cd ..
 rm -rf paru-bin
 
 echo "Installing packages"
-paru -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft noto-fonts alacritty mpv libxinerama xorg-fonts-misc picom lxappearance brightnessctl pavucontrol playerctl pamixer flameshot otf-font-awesome polkit-gnome xclip feh xtrlock gnome-themes-extra papirus-icon-theme blueman
+paru -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft noto-fonts noto-fonts-emoji alacritty mpv libxinerama xorg-fonts-misc picom lxappearance brightnessctl pavucontrol playerctl pamixer flameshot otf-font-awesome polkit-gnome xclip feh xtrlock gnome-themes-extra papirus-icon-theme blueman
 sudo systemctl enable bluetooth
 
 # Thunar
@@ -18,7 +18,7 @@ cd
 
 # Apps
 paru -S obsidian signal-desktop syncthing keepassxc ttf-indic-otf
-paru librewolf-bin
+paru -S --noconfirm librewolf-bin
 clear
 paru -S --noconfirm qview
 clear
