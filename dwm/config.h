1be01703fc6724ef600c15f5b4ee8375c517ec32
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-12 00:41:21 +0600

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browser[]    = { "librewolf", NULL };
+static const char *browser[]    = { "brave", NULL };
 static const char *file[]       = { "thunar", NULL };
 static const char *prtscrcmd[] = { "flameshot", "gui", NULL};
 
