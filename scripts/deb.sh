1374329712c772dd66df1d9810ecaa99164fd6e2
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-14 21:32:09 +0600

    Update deb.sh

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-   exit 1
-fi
+sudo sed -i "1s/.*/Acquire::http::Pipeline-Depth \"$PARALLEL_DOWNLOADS\";/" /etc/apt/apt.conf.d/99parallel-downloads
+sudo apt update
 
-echo 'Acquire::http::Pipeline-Depth "5";' > /etc/apt/apt.conf.d/99parallel
+# DWM packages
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit make libx11-dev libxft-dev libxinerama-dev build-essential fonts-font-awesome fonts-noto-color-emoji libfribidi-dev 
 
-apt update
+sudo apt install --no-install-recommends lxappearance feh picom thunar alacritty flameshot
 
-sudo apt install --no-install-recommends syncthing
+# Pipewire
+ssudo apt install --no-install-recommends pamixer pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio wireplumber pulseaudio-utils 
+
+systemctl --user --now enable wireplumber.service
+
+cd
+git clone https://github.com/mashrukk/dwm
+mv dwm/ wm
+
+echo "Installing dwm, dmenu, and slstatus..."
+cd wm/dwm || exit
+sudo make clean install
+cd ../dmenu || exit
+sudo make clean install
+cd ../slstatus || exit
+sudo make clean install
+cd
+mv wm/xinitrc ~/.xinitrc
+
+echo "Setting up picom"
+cd
+cd /etc/xdg/ || exit
+sudo rm picom.conf
+cd
+sudo mv wm/picom.conf /etc/xdg/
+cd
+mv wm/alacritty ~/.config/
+cd
+mkdir Pictures
+cd Pictures
+mkdir wallpapers
+cd
+mv wm/wall.jpg Pictures/wallpapers
+mkdir Videos
+mkdir Documents
+cd Documents
+mkdir Shared
+cd
+cd wm/scripts
+rm install.sh deb.sh suse.sh
+cd
+startx
