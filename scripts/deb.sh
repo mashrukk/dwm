879e84372a7c6f7c3a5cb26bd455dbd9f6c5a7bd
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-14 21:45:58 +0600

    Update deb.sh

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -4,7 +4,7 @@ sudo sed -i "1s/.*/Acquire::http::Pipeline-Depth \"$PARALLEL_DOWNLOADS\";/" /etc
 sudo apt update
 
 # DWM packages
-sudo apt install --no-install-recommends coreutils xserver-xorg x11-xserver-utils xinit make libx11-dev libxft-dev libxinerama-dev build-essential fonts-noto-color-emoji libfribidi-dev unzip
+sudo apt install --no-install-recommends coreutils xserver-xorg x11-xserver-utils xinit make libx11-dev libxft-dev libxinerama-dev build-essential fonts-noto-color-emoji libfribidi-dev
 
 sudo apt install --no-install-recommends lxappearance feh picom thunar alacritty
 
